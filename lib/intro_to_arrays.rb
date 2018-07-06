4875c6bdb4e592c48056a9ab38fad204f760ed63
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 20:23:37 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,3 +10,4 @@ def first_element (my_first_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 my_first_element = @taylor_swift.first
 end
+def third_element 
